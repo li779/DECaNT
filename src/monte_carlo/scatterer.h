5f457e92c3c9214a59329bcaca6e7015bf5c90bd
li779 <32557523+li779@users.noreply.github.com>
2020-02-14 14:11:56 -0600

    Add files via upload

--- src/monte_carlo/scatterer.h
+++ src/monte_carlo/scatterer.h
@@ -103,7 +103,7 @@ public:
   // TODO: there might be a problem when there is nothing in neighbor list
   void set_max_rate(const double& max_hopping_radius){
     auto neighbors = find_neighbors(max_hopping_radius);
-    if (neighbors == NULL){
+    if (neighbors.empty()){
       _max_rate = 5;
     } else {
       _max_rate = neighbors.back().first;
